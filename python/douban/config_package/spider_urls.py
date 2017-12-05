f59c23c6f592ff4b34644e148761f5e280bf98e1
何森 <1078213694@qq.com>
2017-12-05 23:07:13 +0800

    Add files via upload

---
+++ python/douban/config_package/spider_urls.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 正在上映
+now_playing = [
+    'https://movie.douban.com/cinema/nowplaying/beijing/',
+]
+
+# 即将上映
+coming = [
+    'https://movie.douban.com/coming'
+]
+
+# 选电影
+
+choice_movie = [
+    'https://movie.douban.com/j/search_subjects?type=movie&tag={}&sort=recommend&page_limit=20&page_start=0'
+]
